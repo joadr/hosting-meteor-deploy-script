af9ddcdc2833edd4f7c98cc6c363b2d0520da482
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-05-07 16:26:34 -0300

    added phantomjs deps

--- prepare.sh
+++ prepare.sh
@@ -14,6 +14,11 @@ echo ""
 sudo apt-get install build-essential -y > /dev/null
 sudo apt-get install python2.7 -y > /dev/null
 
+# Install PhantomJS Deps
+sudo apt-get install build-essential g++ flex bison gperf ruby perl \
+  libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
+  libpng-dev libjpeg-dev python libx11-dev libxext-dev -y > /dev/null
+
 # Install Meteor
 echo ""
 echo "====> Installing Meteor..."
